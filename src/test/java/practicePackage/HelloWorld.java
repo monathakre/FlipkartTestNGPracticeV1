f8d131057fe149502d1f2e1e329f473b3ce66ff5
Mona <monathakare111@gmail.com>
2021-05-13 10:46:43 +0530

    ** Commiting changes made from workspace **

--- src/test/java/practicePackage/HelloWorld.java
+++ src/test/java/practicePackage/HelloWorld.java
@@ -1,1 +1,12 @@
 //Test cases added by other team member
+//Now updating class from workspace
+package practicePackage;
+
+class HelloWorld{
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		
+	}
+}
